a2ed057431511ebe812f0c3c2259d5b928939c0e
zabcd-ai <zabcd3537@gmail.com>
2026-01-06 02:11:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,7 +33,7 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 
 # auth_channel means force subscribe channel.
 # if REQUEST_TO_JOIN_MODE is true then force subscribe work like request to join fsub, else if false then work like normal fsub.
-REQUEST_TO_JOIN_MODE = bool(environ.get('REQUEST_TO_JOIN_MODE', False)) # Set True Or False
+REQUEST_TO_JOIN_MODE = bool(environ.get('https://t.me/IO_QNews', True)) # Set True Or False
 TRY_AGAIN_BTN = bool(environ.get('TRY_AGAIN_BTN', False)) # Set True Or False (This try again button is only for request to join fsub not for normal fsub)
 
 # This Is Force Subscribe Channel, also known as Auth Channel 
@@ -41,7 +41,7 @@ auth_channel = environ.get('AUTH_CHANNEL', '') # give your force subscribe chann
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 
 # This Channel Is For When User Request Any File Name With command or hashtag like - /request or #request
-reqst_channel = environ.get('REQST_CHANNEL', '')
+reqst_channel = environ.get('REQST_CHANNEL', '1003536118990')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 
 # This Channel Is For Index Request 
@@ -88,7 +88,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'apmeqtvydkjs8') # Publ
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+dXv_0B3K-6gyNGE1')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/MarvelitesSyndicate')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/IO_QNews')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'MarvelitesSyndicate') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', '')
@@ -196,6 +196,7 @@ else:
 # Don't Remove Credit @VJ_Bots
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
